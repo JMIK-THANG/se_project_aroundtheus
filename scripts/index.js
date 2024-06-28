7cc44ba6fa31575d8f428f5fd69c7adddfedd299
Jmik Thang <jmikthang5@gmail.com>
2024-06-28 15:38:43 -0400

    modal js

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,37 @@
 const initialCards = [
-      {
-            name:"Yosemite Valley",
-            link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-      },
-      {
-            name:"Lake Louise",
-            link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-      },
-      {
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
 
-            name:"Bald Mountains",
-            link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-      },
-      {
-            name:"Latemar",
-            link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-      },
-      {
-            name:"Vanoise National Park",
-            link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-      },
-      { 
-            name:"Lago di Braies",
-            link:"https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-      }
-]; 
+const profileEditButton = document.getElementById("profile__edit-button");
+const profileEditModal = document.getElementById("profile__edit-modal");
+const profileModalClose = document.getElementById("modal__close");
+console.log(profileEditModal);
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+profileModalClose.addEventListener("click", function () {
+  profileEditModal.classList.remove("modal_opened");
+});
